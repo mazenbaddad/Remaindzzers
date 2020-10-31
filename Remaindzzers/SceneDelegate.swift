ae81ef5b15ae97721414e9067597b36b1d05cb02
mazenbaddad <mazenbaddad@hotmail.com>
2020-10-31 23:23:58 +0200

    init commit

---
+++ Remaindzzers/SceneDelegate.swift
@@ -0,0 +1,41 @@
+//
+//  SceneDelegate.swift
+//  Remaindzzers
+//
+//  Created by mazen baddad on 10/31/20.
+//  Copyright © 2020 mazen baddad. All rights reserved.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+
+    @available(iOS 13.0, *)
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
+        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
+        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        window = UIWindow(frame: windowScene.coordinateSpace.bounds)
+        window?.windowScene = windowScene
+
+//        window?.rootViewController = ViewController()
+//        window?.makeKeyAndVisible()
+    }
+
+    @available(iOS 13.0, *)
+    func sceneDidEnterBackground(_ scene: UIScene) {
+        // Called as the scene transitions from the foreground to the background.
+        // Use this method to save data, release shared resources, and store enough scene-specific state information
+        // to restore the scene back to its current state.
+
+        // Save changes in the application's managed object context when the application transitions to the background.
+        (UIApplication.shared.delegate as? AppDelegate)?.saveContext()
+    }
+
+
+}
+
